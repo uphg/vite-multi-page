c7085da6963c5822c73a995429c346c67fbd4e2f
lvheng <1834805770@qq.com>
2024-07-16 14:18:37 +0800

    feat: 根据项目目录打包

--- packages/foo/main.ts
+++ packages/foo/main.ts
@@ -2,7 +2,7 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
     <h1>Foo</h1>
     <div class="card">
-      <button id="counter" type="button"></button>
+      <button id="counter" type="button">点我</button>
     </div>
     <p class="read-the-docs">
       Click on the Vite and TypeScript logos to learn more
